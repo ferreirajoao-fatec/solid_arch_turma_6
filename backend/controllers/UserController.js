be0e14c3438a441156dd665806b41e6d6de6ad34
ferreirajoao-fatec <joao.ferreira113@fatec.sp.gov.br>
2026-04-08 12:46:51 -0300

    Teste do domínio e rotas com controllers

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,7 @@
+// const User = require('../models/User')
+
+module.exports = class UserController{
+    static async register(req, rea){
+        res.json('Olá Get Pet')
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const UserRouters = require('./routers/UserRouters')
 
 const app = express()
 
@@ -8,5 +9,7 @@ app.use(express.json())
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 
 app.use(express.static('public'))
+
+app.use('/users', UserRouters)
 
 app.listen(5000)
